63e2f708c03a635f5e8185e3128350193da180d0
sai45 <saikiran.tanniru45@gmail.com>
2024-03-06 20:49:43 +0530

    adding recent java files

---
+++ patabcde.java
@@ -0,0 +1,10 @@
+class Pattern{
+	public static void main(String args[]){
+		for(char x='A';x<='E';x++){
+			for(char t='A';t<='E';t++){
+				System.out.print(t);
+				}
+			System.out.println();
+				}
+		}
+}
